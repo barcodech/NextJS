d17fabb9d7d262734230a80b172a6201a0f2a8b2
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 10:03:44 +0700

    Add files via upload

---
+++ pages/barcode/test.js
@@ -0,0 +1,10 @@
+const test = () => {
+    return (
+        <div className="test">
+            <h2>Test</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio ipsa voluptates id sint recusandae eos, cumque delectus obcaecati nulla quam.</p>
+        </div>
+     );
+}
+ 
+export default test;
