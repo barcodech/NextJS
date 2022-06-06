57bffe487c644ef0f9de5ffeb42175605b76bb01
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 10:31:19 +0700

    Add files via upload

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+ 
+const about = () => {
+    return (
+        <>
+        <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+         </Head>
+        <div className="about">
+            <h2>about</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quae. Nam cum totam recusandae ad est dolorum ipsum, et ducimus!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quae. Nam cum totam recusandae ad est dolorum ipsum, et ducimus!</p>
+        </div>
+        </>
+     );
+}
+ 
+export default about;
