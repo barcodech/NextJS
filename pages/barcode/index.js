bfb852f04f7e7cf2ff432f46b6731c785b941f84
barcodech <103811940+barcodech@users.noreply.github.com>
2022-06-06 10:24:08 +0700

    Add files via upload

---
+++ pages/barcode/index.js
@@ -0,0 +1,10 @@
+const index = () => {
+    return (
+        <div className="index">
+            <h2>Index</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptate nemo nostrum, tempora explicabo asperiores unde atque facilis adipisci quis!</p>
+        </div>
+     );
+}
+ 
+export default index;
